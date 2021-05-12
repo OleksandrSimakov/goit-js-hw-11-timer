9137687523939e1087a12e9ba44babbaf2f92f44
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-05-13 02:13:15 +0300

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -193,6 +193,36 @@ module.hot.accept(reloadCSS);
 "use strict";
 
 require("./styles.css");
+
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2021')
+});
+/*
+ * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+ * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+ */
+
+var days = Math.floor(time / (1000 * 60 * 60 * 24));
+/*
+ * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+ * остатка % и делим его на количество миллисекунд в одном часе
+ * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+ */
+
+var hours = Math.floor(time % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+/*
+ * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+ * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+ */
+
+var mins = Math.floor(time % (1000 * 60 * 60) / (1000 * 60));
+/*
+ * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+ * миллисекунд в одной секунде (1000)
+ */
+
+var secs = Math.floor(time % (1000 * 60) / 1000);
 },{"./styles.css":"styles.css"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -221,7 +251,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60995" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64602" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
