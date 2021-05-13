7ad0e4495a1dd981e1d5578f6ea760fb24485425
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-05-13 13:43:56 +0300

    adds corrections to timer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 import './styles.css';
 
+const refs = {
+  clockfaceDays: document.querySelector('span[data-value="days"]'),
+  clockfaceHours: document.querySelector('span[data-value="hours"]'),
+  clockfaceMins: document.querySelector('span[data-value="mins"]'),
+  clockfaceSecs: document.querySelector('span[data-value="secs"]'),
+}
+
 const timer = {
   start() {
-    const targetDate = new Date('Jul 17, 2022');
-    setInterval(() => {
+    const targetDate = new Date('May 13, 2022');
+    const intervalId = setInterval(() => {
       const currentDate = Date.now();
       const deltaTime = targetDate - currentDate;
       const {days, hours, mins, secs} = getTimeComponents(deltaTime);
-      console.log(`${ days }:${ hours }:${ mins }:${ secs }`);
+      // console.log(`${days}:${hours}:${mins}:${secs}`);
+      updateClockface({days, hours, mins, secs});
+      if (deltaTime <= 0) {
+        clearInterval(intervalId);
+        console.log('Акция окончена')
+};
     }, 1000);
   }
 }
 
 timer.start()
-
-// class CountdownTimer {
-//   constructor() {
-//   }
-// }
 
 // new CountdownTimer({
 //   selector: '#timer-1',
@@ -34,4 +41,11 @@ function getTimeComponents(time) {
 
 function pad(value) {
   return String(value).padStart(2,'0')
+}
+
+function updateClockface({days, hours, mins, secs}) {
+  refs.clockfaceDays.textContent = `${days}`;
+  refs.clockfaceHours.textContent = `${hours}`;
+  refs.clockfaceMins.textContent = `${mins}`;
+  refs.clockfaceSecs.textContent = `${secs}`;
 }
