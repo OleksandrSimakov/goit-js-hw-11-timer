a9aad4e48cc4743449b97b01d4afe487db80e704
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-05-13 12:35:13 +0300

    corrects timer

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
 import './styles.css';
 
-new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2021'),
-});
+const timer = {
+  start() {
+    const targetDate = new Date('Jul 17, 2022');
+    setInterval(() => {
+      const currentDate = Date.now();
+      const deltaTime = targetDate - currentDate;
+      const {days, hours, mins, secs} = getTimeComponents(deltaTime);
+      console.log(`${ days }:${ hours }:${ mins }:${ secs }`);
+    }, 1000);
+  }
+}
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+timer.start()
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// class CountdownTimer {
+//   constructor() {
+//   }
+// }
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+// new CountdownTimer({
+//   selector: '#timer-1',
+//   targetDate = new Date('Jul 17, 2021');
+// });
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+function getTimeComponents(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  return {days, hours, mins, secs}
+}
+
+function pad(value) {
+  return String(value).padStart(2,'0')
+}
